1a6b541a7ce4a036a5fbc1a10c8acb697ad6ab7e
Fi <filipkacic@protonmail.com>
2024-11-18 21:16:45 +0100

    navbar space adjustment and download pd fbtn try 02

--- assets/js/download_btn.js
+++ assets/js/download_btn.js
@@ -1,6 +1,6 @@
 document.getElementById("download_btn").addEventListener("click", function() {
     const link = document.createElement("a");
-    link.href = "../docs/degree.pdf";
+    link.href = "assets/docs/degree.pdf";
     link.download = "degree.pdf";
     link.click();
 });
