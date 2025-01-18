6f411473f8ee46a8ccef0dc3ac870aaaa539128d
Fi <filipkacic@protonmail.com>
2025-01-18 10:27:25 +0100

    final work

--- assets/js/download_btn.js
+++ assets/js/download_btn.js
@@ -1,6 +1,11 @@
-document.getElementById("download_btn").addEventListener("click", function() {
-    const link = document.createElement("a");
-    link.href = "assets/docs/degree.pdf";
-    link.download = "degree.pdf";
-    link.click();
-});
+function setupDownload(buttonId, filePath, fileName) {
+    document.getElementById(buttonId).addEventListener("click", function() {
+        const link = document.createElement("a");
+        link.href = filePath;
+        link.download = fileName;
+        link.click();
+    });
+}
+
+setupDownload("download_btn", "assets/docs/degree.pdf", "degree.pdf");
+setupDownload("download_btn_2", "assets/docs/kacic_filip_zavrsni_rad.pdf", "final_work.pdf");
